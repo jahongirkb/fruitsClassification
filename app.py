a8773466f63e1c4031300c7331d841508bdcb49f
Jahongir Karimberdiyev <92970540+jahongirkb@users.noreply.github.com>
2022-05-13 13:27:30 +0500

    new files added

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+st.title("Mevalarni klassifikatsiya qiluvchi model")
+
+file = st.file_uploader("Rasmni yuklash", type=['jpg', 'png', 'gif'])
+if file:
+    st.image(file)
+
+    img = PILImage.create(file)
+
+    model = load_learner('fruits_model.pkl')
+
+    pred, pred_id, probs = model.predict(img)
+    prob = probs[pred_id]*100
+    if prob>98:
+        st.success(f"Bashorat: {pred}")
+        st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+        fig = px.bar(x=probs*100, y=model.dls.vocab)
+        st.plotly_chart(fig)
+    else:
+        st.info("Bunday rasm modelda mavjud emas!")
